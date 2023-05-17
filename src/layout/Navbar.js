dacc41e0f77458699f092a7261c257687101b0c4
Anish Kumar <gsranish@gmail.com>
2023-05-17 21:04:17 +0530

    new

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -15,6 +15,7 @@ export default function Navbar() {
         <a class="nav-link" href="#">Features</a>
         <a class="nav-link" href="#">Pricing</a>
         <a class="nav-link disabled">Disabled</a>
+        
       </div>
     </div>
   </div>
